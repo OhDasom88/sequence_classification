916dc81320a7df50a1424dd3091dacd6868d73f3
dasomoh <dasomoh88@gmail.com>
2022-02-28 01:03:45 +0000

    preprocessing text embedding

--- inference/dp/NLI_Data_preprocessing.py
+++ inference/dp/NLI_Data_preprocessing.py
@@ -7,30 +7,30 @@ defaultDir = re.search(r'.+(?=sequence_classification)',__file__).group(0)+'sequ
 
 # sent_id = -1
 # examples = []
-for name in ('test', 'train'):# KLUE
-# for name in ('test',):# DACON
+# for name in ('test', 'train'):# KLUE
+for name in ('test',):# DACON
     sent_id = -1
     newCols = []
 
-    with open(f'{defaultDir}/data/dacon/klue-nli-v1_1_{name}_json.json') as f:
-    # with open(f'{defaultDir}/data/dacon/{name}_data_csv.json') as f:
+    # with open(f'{defaultDir}/data/dacon/klue-nli-v1_1_{name}_json.json') as f:
+    with open(f'{defaultDir}/data/dacon/{name}_data_csv.json') as f:
         data = json.load(f)
 
     # # /home/dasomoh88/sequence_classification/data/dacon/klue-nli-v1.1_test.json
-    with open(f'{defaultDir}/data/dacon/klue-nli-v1_1_{name}.json') as f:
-        dataOrigin = json.load(f)
+    # with open(f'{defaultDir}/data/dacon/klue-nli-v1_1_{name}.json') as f:
+    #     dataOrigin = json.load(f)
 
-    # dataOrigin = pd.read_csv(f'{defaultDir}/data/dacon/{name}_data.csv')
+    dataOrigin = pd.read_csv(f'{defaultDir}/data/dacon/{name}_data.csv')
 
     # Dacon
-    # for i, row in tqdm(enumerate(dataOrigin.itertuples())):
+    for i, row in tqdm(enumerate(dataOrigin.itertuples())):
     # KLUE
-    for i, row in tqdm(enumerate(dataOrigin)):
+    # for i, row in tqdm(enumerate(dataOrigin)):
         newRow = []
         newCols.append(newRow)
-        newRow.append(data[sent_id][-1])# tag representaion
-        # for col in [2,3]:#csv DACON <<  premise, hypothesis
-        for col in ['premise','hypothesis']:# json KLUE << premise, hypothesis
+        
+        for col in [2,3]:#csv DACON <<  premise, hypothesis
+        # for col in ['premise','hypothesis']:# json KLUE << premise, hypothesis
             sent_id += 1
             sent = []
             word_id = 0
@@ -54,9 +54,10 @@ for name in ('test', 'train'):# KLUE
                     sent.append(words[word_id])
                     word_id+=1
             newRow.append(' [word] '.join(sent))
+            newRow.append(data[sent_id][-1])# tag representaion
 
     # pd.concat([df, pd.DataFrame(newCols)], axis=1).to_csv(f'{defaultDir}/data/dacon/{name}_from_klue_new_with_dp.csv')
     # json from klue
-    pd.concat([pd.DataFrame(dataOrigin), pd.DataFrame(newCols)], axis=1).to_csv(f'{defaultDir}/data/dacon/{name}_from_klue_new_with_dp_v2.csv')
+    # pd.concat([pd.DataFrame(dataOrigin), pd.DataFrame(newCols)], axis=1).to_csv(f'{defaultDir}/data/dacon/{name}_from_klue_new_with_dp_v2.csv')
     # csv from dacon
-    # pd.concat([dataOrigin, pd.DataFrame(newCols)], axis=1).to_csv(f'{defaultDir}/data/dacon/{name}_from_dacon_with_dp_v2.csv')
+    pd.concat([dataOrigin, pd.DataFrame(newCols)], axis=1).to_csv(f'{defaultDir}/data/dacon/{name}_from_dacon_with_dp_v2.csv')
